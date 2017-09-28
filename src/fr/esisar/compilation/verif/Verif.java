037f8debb8ca6a5c914c7331064918ccce9b7513
Haashii <baptiste.pontanier@laposte.net>
2017-09-28 12:33:59 +0200

    Creation template des regles de verifications

--- src/fr/esisar/compilation/verif/Verif.java
+++ src/fr/esisar/compilation/verif/Verif.java
@@ -58,15 +58,129 @@ public class Verif {
     **************************************************************************/
    private void verifier_LISTE_DECL(Arbre a) throws ErreurVerif {
       // A COMPLETER
+	   switch(a.getNoeud()) {
+	   	case ListeDecl:
+	   		verifier_LISTE_DECL(a.getFils1());
+	   		verifier_DECL(a.getFils2());
+	   		break;
+	   	case Vide:
+	   		break;
+	   	default:
+	   		throw new ErreurInterneVerif(
+	   				"Arbre incorrect dans verifier_LISTE_DECL");
+	   }
    }
-
+   
+   /**************************************************************************
+    * DECL
+    **************************************************************************/
+   
+   private void verifier_DECL(Arbre a) throws ErreurVerif{
+	   // A COMPLETER
+   }
+   
    /**************************************************************************
     * LISTE_INST
     **************************************************************************/
    private void verifier_LISTE_INST(Arbre a) throws ErreurVerif {
       // A COMPLETER
    }
-
+   
+   /**************************************************************************
+    * INST
+    **************************************************************************/
+   
+   private void verifier_INST(Arbre a) throws ErreurVerif{
+	   // A COMPLETER
+   }
+   
+   /**************************************************************************
+    * LISTE_IDF
+    **************************************************************************/
+   
+   private void verifier_LIST_IDF(Arbre a) throws ErreurVerif{
+	   // A COMPLETER
+   }
+   
+   /**************************************************************************
+    * IDF
+    **************************************************************************/
+   
+   private void verifier_IDF(Arbre a) throws ErreurVerif{
+	   // A COMPLETER
+   }
+   
+   /**************************************************************************
+    * TYPE
+    **************************************************************************/
+   
+   private void verifier_TYPE(Arbre a) throws ErreurVerif{
+	   // A COMPLETER
+   }
+   
+   /**************************************************************************
+    * TYPE_INTERVALLE
+    **************************************************************************/
+   
+   private void verifier_TYPE_INTERVALLE(Arbre a) throws ErreurVerif{
+	   // A COMPLETER
+   }
+   
+   /**************************************************************************
+    * CONSTANTE
+    **************************************************************************/
+   
+   private void verifier_CONSTANTE(Arbre a) throws ErreurVerif{
+	   // A COMPLETER
+   }
+   
+   /**************************************************************************
+    * CONST
+    **************************************************************************/
+   
+   private void verifier_CONST(Arbre a) throws ErreurVerif{
+	   // A COMPLETER
+   }
+   
+   /**************************************************************************
+    * PAS
+    **************************************************************************/
+   
+   private void verifier_PAS(Arbre a) throws ErreurVerif{
+	   // A COMPLETER
+   }
+   
+   /**************************************************************************
+    * LISTE_EXP
+    **************************************************************************/
+   
+   private void verifier_LISTE_EXP(Arbre a) throws ErreurVerif{
+	   // A COMPLETER
+   }
+   
+   /**************************************************************************
+    * EXP
+    **************************************************************************/
+   
+   private void verifier_EXP(Arbre a) throws ErreurVerif{
+	   // A COMPLETER
+   }
+   
+   /**************************************************************************
+    * FACTEUR
+    **************************************************************************/
+   
+   private void verifier_FACTEUR(Arbre a) throws ErreurVerif{
+	   // A COMPLETER
+   }
+   
+   /**************************************************************************
+    * PLACE
+    **************************************************************************/
+   
+   private void verifier_PLACE(Arbre a) throws ErreurVerif{
+	   // A COMPLETER
+   }
    // ------------------------------------------------------------------------
    // COMPLETER les operations de vérifications et de décoration pour toutes 
    // les constructions d'arbres
