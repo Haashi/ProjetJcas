7fc02579273c5b8a16db36ecfb86463518e703c6
XtremElite <Jordan.Legland@etu.esisar.grenoble-inp.fr>
2017-10-29 17:08:55 +0100

    new_line et wstr

--- src/fr/esisar/compilation/gencode/Generation.java
+++ src/fr/esisar/compilation/gencode/Generation.java
@@ -13,17 +13,35 @@ class Generation {
     * Méthode principale de génération de code.
     * Génère du code pour l'arbre décoré a.
     */
-   static Prog coder(Arbre a) {
-      Prog.ajouterGrosComment("Programme généré par JCasc");
-
-      // -----------
-      // A COMPLETER
-      // -----------
-
-
+	static Prog coder(Arbre a) {
+	  Prog.ajouterGrosComment("Programme généré par JCasc");
+      Arbre currentNode = a; 
+      Arbre previousNode ;
+      String chaine;
+      Inst inst;
+      
+      System.out.println(a.getNoeud());
+      previousNode = currentNode;
+      currentNode = currentNode.getFils2().getFils2();
+      System.out.println(currentNode.getNoeud());
+      
+      switch(currentNode.getNoeud())
+      {
+      	case Ligne: Prog.ajouter(Inst.creation0(Operation.WNL));
+      				break;
+      	
+      	case Ecriture: inst = Inst.creation1(Operation.WSTR, Operande.creationOpChaine(currentNode.getFils1().getFils2().getChaine().substring(1,currentNode.getFils1().getFils2().getChaine().length()-1)));
+      					System.out.println( Operande.creationOpChaine(currentNode.getFils1().getFils2().getChaine()));
+      				   Prog.ajouter(inst);
+      				   break;
+      	
+      				
+      	default:
+      		System.err.println("Unknown node");
+      }
       // Fin du programme
       // L'instruction "HALT"
-      Inst inst = Inst.creation0(Operation.HALT);
+      inst = Inst.creation0(Operation.HALT);
       // On ajoute l'instruction à la fin du programme
       Prog.ajouter(inst);
 
