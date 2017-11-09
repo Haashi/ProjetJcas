8ce0e7276b845b3678b0855415ec94a9b5ca4067
XtremElite <Jordan.Legland@etu.esisar.grenoble-inp.fr>
2017-11-09 11:13:40 +0100

    Presque fini le code recherche decl

---
+++ src/fr/esisar/compilation/gencode/Adresse.java
@@ -0,0 +1,105 @@
+package fr.esisar.compilation.gencode;
+
+
+import fr.esisar.compilation.global.src.NatureType;
+import fr.esisar.compilation.global.src.Type;
+import java.util.Hashtable;
+import java.util.Enumeration;
+
+
+public class Adresse {
+	
+
+	/** 
+	 * Un environnement qui permet d'associer des adresses à des identifiants.
+	 */
+
+	   /**
+	    * La table qui contient l'environnement
+	    */
+	   private final Hashtable<String, Integer> Memoire;
+	   private static int offset;
+	   /**
+	    * Constructeur d'environnement. 
+	    */
+	   public Adresse() { 
+	      this.Memoire = new Hashtable<String, Integer>();
+	   }
+
+	   /**
+	    * Enrichissement de l'environnement avec le couple (s, Integer).
+	    * <ul>
+	    * <li> Si l'identifiant s est présent dans l'environnement, 
+	    *      enrichir(s, Integer) ne fait rien et retourne false. </li>
+	    * <li> Si l'identifiant s n'est pas présent dans l'environnement, 
+	    *      enrichir(s, Integer) ajoute l'association (s, Integer) dans 
+	    *      l'environnement et retourne true. </li>
+	    * </ul>
+	    */
+	   private boolean enrichir(String s, Integer adresse) {
+	      if (Memoire.containsKey(s)) {
+	         return false;
+	      } else {
+	         Memoire.put(s, adresse);
+	         return true;
+	      }
+	   }
+
+	   	public void allouer(String id,Type type,Borne... borne) {
+	   		if(type.getNature()!=NatureType.Array) {
+	   			offset++;
+	   			boolean ajouter = this.enrichir(id, offset);
+	   			if(!ajouter) {
+	   				System.out.println("Erreur dans l'ajout adresse");
+	   			}
+	   		}
+	   		else {
+	   			allouerTableau(id,borne);
+	   		}
+	   	}
+	   	
+	   	public void allouerTableau(String id,Borne...borne) {
+	   		if(borne.length==1) {
+	   			for(int j= borne[0].getBorneInf();j<=borne[0].getBorneSup();j++) {
+   					offset++;
+   					boolean ajouter = this.enrichir(id.concat("["+j+"]"), offset);
+   					if(!ajouter) {
+   						System.out.println("Erreur lors de l'ajout adresse tableau");
+   					}
+   				}
+	   		}
+	   		else {
+	   			Borne newBorne[]= new Borne[borne.length-1];
+	   			int i=0;
+	   			for(int j=1;j<borne.length;j++) {
+	   				newBorne[i]=borne[j];
+	   				i++;
+	   			}
+	   			for(i=borne[0].getBorneInf();i<=borne[0].getBorneSup();i++) {
+	   				allouerTableau(id.concat("["+i+"]"),newBorne);
+	   			}
+	   		}
+	   	}
+	   /**
+	    * Cherche la defn associée à la chaîne s dans l'environnement.
+	    * Si la chaîne s n'est pas dans l'environnement, chercher(s) 
+	    * retourne null.
+	    */
+	   public Integer chercher(String s,Integer... indices) {
+		   for (int i=0;i<indices.length;i++) {
+			   s=s.concat("["+indices[i]+"]");
+		   }
+	      return Memoire.get(s);
+	   }
+	   public void afficher() {
+		     Enumeration<String> keys = Memoire.keys();
+		     while (keys.hasMoreElements()) {
+		    	String id = keys.nextElement();
+		        String s = "CHAINE : " + id + " --> ADRESSE : ";
+		        Integer def = Memoire.get(id);
+		        System.out.println(s + def);
+		     }
+	   }
+	   
+	 
+}
--- src/fr/esisar/compilation/gencode/Generation.java
+++ src/fr/esisar/compilation/gencode/Generation.java
@@ -2,6 +2,7 @@ package fr.esisar.compilation.gencode;
 
 import fr.esisar.compilation.global.src.*;
 import fr.esisar.compilation.global.src3.*;
+import java.util.ArrayList;
 
 /**
  * Génération de code pour un programme JCas à partir d'un arbre décoré.
@@ -20,6 +21,10 @@ class Generation {
       String chaine;
       Inst inst;
       
+      //Appeler le parcours des déclarations en passant la classe qui répertorie les offsets de la pile et la racine arbre
+      parcoursDecl(a);
+      
+      
       System.out.println(a.getNoeud());
       previousNode = currentNode;
       currentNode = currentNode.getFils2().getFils2();
@@ -27,13 +32,17 @@ class Generation {
       
       switch(currentNode.getNoeud())
       {
+      
+      	case Vide: break;
+    	  
+      
       	case Ligne: Prog.ajouter(Inst.creation0(Operation.WNL));
       				break;
       	
-      	case Ecriture: inst = Inst.creation1(Operation.WSTR, Operande.creationOpChaine(currentNode.getFils1().getFils2().getChaine().substring(1,currentNode.getFils1().getFils2().getChaine().length()-1)));
-      					System.out.println( Operande.creationOpChaine(currentNode.getFils1().getFils2().getChaine()));
-      				   Prog.ajouter(inst);
-      				   break;
+      	case Ecriture:  inst = Inst.creation1(Operation.WSTR, Operande.creationOpChaine(currentNode.getFils1().getFils2().getChaine().substring(1,currentNode.getFils1().getFils2().getChaine().length()-1)));
+      					System.out.println(Operande.creationOpChaine(currentNode.getFils1().getFils2().getChaine()));
+      					Prog.ajouter(inst);
+      					break;
       	
       				
       	default:
@@ -48,6 +57,86 @@ class Generation {
       // On retourne le programme assembleur généré
       return Prog.instance(); 
    }
+	
+	
+	static void parcoursDecl(Arbre a)
+	{//Parcours de la liste des déclarations, pour chaque Identificateur on lui associe un offset par rapport à GB pour le placer dans la pile
+		Arbre currentNode = a;
+		Noeud node = currentNode.getNoeud();
+		
+		//currentNode = currentNode.getFils2();
+		//System.out.println("Hello" + currentNode.getNoeud());
+		
+		if(node == Noeud.Programme)parcoursDecl(currentNode.getFils1());
+		else if(node == Noeud.ListeDecl)
+		{
+			parcoursDecl(currentNode.getFils1());
+			parcoursDecl(currentNode.getFils2());
+		}
+		else if(node == Noeud.Vide)return;
+		else if(node == Noeud.Decl)
+		{
+			parcoursDecl(currentNode.getFils1());
+			if(currentNode.getFils2().getNoeud() == Noeud.Tableau)
+			{
+				ArrayList<String> nom = searchIdent(currentNode.getFils1(), new ArrayList());
+				//ArrayList<Borne> borne = searchBorne(currentNode, new ArrayList());
+				
+				
+			}
+		}
+		else if(node == Noeud.ListeIdent)
+		{
+			parcoursDecl(currentNode.getFils1());
+			parcoursDecl(currentNode.getFils2());
+		}
+		else if(node == Noeud.Ident)
+		{
+			//Call stockage de l'ident pour les variables de type boolean, integer, real
+		
+		}
+		else if(node == Noeud.Tableau)
+		{
+			
+			
+		}
+	}
+
+	static ArrayList<String> searchIdent (Arbre a, ArrayList<String> s)
+	{
+		Arbre currentNode = a;
+		Noeud node = currentNode.getNoeud();
+		if(node == Noeud.ListeIdent)
+		{
+			s = searchIdent(currentNode.getFils1(), s);
+			s = searchIdent(currentNode.getFils2(), s);
+			return(s);
+		}
+		else if (node == Noeud.Vide)return(s);
+		else if (node == Noeud.Ident)
+		{
+			s.add(currentNode.getChaine());
+			return(s);
+		}
+		return(s);
+	}
+	
+	static ArrayList<Borne> searchBorne (Arbre a, ArrayList<Borne> b)
+	{
+		Arbre currentNode = a;
+		Noeud node = currentNode.getNoeud();
+		if(node == Noeud.Tableau)
+		{
+			b.add(new Borne());
+			b = searchBorne(currentNode.getFils1(), b);
+			b = searchBorne(currentNode.getFils2(), b);
+			return(b);
+		}
+		return(b);
+	}
+	
+
+
 }
 
 
